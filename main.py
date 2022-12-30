afc785b630eb2a0dc9b3adcddcad35fdcb6696fb
Bruna Paolo <bruns2506@gmail.com>
2022-12-30 15:54:20 +0100

    updated mian.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#TODO fixare tempo, displaya sia tempo che giorno
+
+from datetime import date
+import time
+
+while True:
+
+#asks for input
+    choice = input("Would you wish to know date or time?\ntype d/t\nor press e to quit.\n")
+
+#gets the date
+    date = date.today()
+#gets the time
+    ti = time.localtime()
+#sets time format
+    current_time = time.strftime("%H:%M:%S" , ti)
+
+
+    if(choice == 'd'):
+        print(date)
+    elif(choice == 't'):
+        print(current_time)
+    elif(choice == 'e'):
+        quit()
+    else:
+        print('Wrong choice, pick either d for date or t for time\n')
+
+
